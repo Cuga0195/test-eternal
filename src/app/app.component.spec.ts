9d1170abf4a7ca37ca1c214ff207684f242e4bd1
Cuga <cuga.03.10@gmail.com>
2025-01-24 13:20:40 -0500

    confirm

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-proyect-eternal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('pruba final publicate');
   });
 });
